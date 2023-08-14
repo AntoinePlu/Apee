9e37b954471395bc43af10f7874a65a852da6fd4
François Vaux <francois@yapok.org>
2023-08-14 10:56:11 +0200

    feat: Add keystatic backend for bookmarks and basic UI

--- keystatic.config.tsx
+++ keystatic.config.tsx
@@ -110,10 +110,77 @@ export default config({
             description: "The name of the bookmark",
           },
         }),
+        url: fields.url({
+          label: "URL",
+          description: "The bookmark URL",
+        }),
         collection: fields.relationship({
           label: "Collection",
           description: "The collection this bookmark should be part of",
           collection: "collections",
+        }),
+        curatedOn: fields.date({
+          label: "Curated on",
+          description: "When was the bookmark curated",
+          defaultValue: { kind: "today" },
+        }),
+        postedOn: fields.date({
+          label: "Posted on",
+          description: "When was the bookmark originally posted",
+        }),
+        author: fields.relationship({
+          label: "Author",
+          description: "The original author for this bookmark",
+          collection: "authors",
+        }),
+        description: fields.document({
+          label: "Description",
+          formatting: true,
+          links: true,
+        }),
+        picture: fields.image({
+          label: "Picture",
+          directory: "public/images/bookmarks/",
+          publicPath: "/images/bookmarks/",
+        }),
+        tags: fields.array(
+          fields.relationship({
+            label: "Tag",
+            collection: "tags",
+          }),
+          {
+            label: "Tags",
+            description: "The tags to associate this bookmark to",
+            itemLabel: (props) => props.value || "Unknown tag",
+          },
+        ),
+      },
+    }),
+    authors: collection({
+      label: "Authors",
+      slugField: "name",
+      path: "src/content/authors/*",
+      schema: {
+        name: fields.slug({
+          name: {
+            label: "Name",
+            description: "The name of the author",
+          },
+        }),
+        picture: fields.image({
+          label: "Picture",
+          directory: "public/images/authors/",
+          publicPath: "/images/authors/",
+        }),
+      },
+    }),
+    tags: collection({
+      label: "Tags",
+      slugField: "name",
+      path: "src/content/tags/*",
+      schema: {
+        name: fields.slug({
+          name: { label: "Name", description: "The tag name" },
         }),
       },
     }),
--- src/app/(website)/collections/[slug]/page.tsx
+++ src/app/(website)/collections/[slug]/page.tsx
@@ -1,3 +1,73 @@
-export default function CollectionPage() {
-  return <div></div>;
+import { Heading } from "@apee/components/ui";
+import { keystatic } from "@apee/lib/keystatic";
+import { DocumentRenderer } from "@keystatic/core/renderer";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+type CollectionPageProps = {
+  params: {
+    slug: string;
+  };
+};
+export default async function CollectionPage({ params }: CollectionPageProps) {
+  const collection = await keystatic.collections.collections.read(params.slug);
+  const bookmarks = await keystatic.collections.bookmarks.all();
+
+  if (collection === null) {
+    notFound();
+  }
+
+  const collectionBookmarks = bookmarks.filter(
+    (bookmark) => bookmark.entry.collection === params.slug,
+  );
+
+  const description = await collection.description();
+
+  return (
+    <div className="flex flex-1 flex-col gap-y-32 overflow-auto px-32 py-20">
+      <header className="grid grid-cols-4 px-24">
+        <div className="col-span-3 flex flex-col gap-y-6">
+          <Heading level={1}>{collection.name}</Heading>
+          <DocumentRenderer
+            document={description}
+            renderers={{
+              block: {
+                paragraph: ({ children }) => (
+                  <p className="text-base text-white-11">{children}</p>
+                ),
+              },
+              inline: {
+                link: ({ children, href }) => (
+                  <Link className="text-white" href={href}>
+                    {children}
+                  </Link>
+                ),
+              },
+            }}
+          />
+        </div>
+        <div />
+      </header>
+
+      <div className="grid grid-cols-3 gap-6">
+        {collectionBookmarks.map((bookmark) => (
+          <div key={bookmark.slug}>
+            {bookmark.entry.picture ? (
+              <Image
+                className="rounded-lg"
+                src={bookmark.entry.picture}
+                width={300}
+                height={200}
+                alt={""}
+              />
+            ) : (
+              <div className="">No picture</div>
+            )}
+            <p className="p-2 text-white-11">By {bookmark.entry.author}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
